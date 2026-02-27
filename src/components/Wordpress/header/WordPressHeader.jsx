62d8a8ab9931092a81f30b37ec0753a8e737d381
FilipeGrimme <filipe.grimme@hotmail.com>
2026-02-27 13:59:03 -0600

    Created a data file for the header and used it in the header component.

--- src/components/Wordpress/header/WordPressHeader.jsx
+++ src/components/Wordpress/header/WordPressHeader.jsx
@@ -1,6 +1,20 @@
 
+import { query } from "express";
+import {NavData} from "./NavData";
+import { useState,useEffect,useRef } from "react";
 
 export default function WordPressHeader() {
+
+  const [openDropdown, setOpenDropdown] = useState(null);
+  const dropdownRefs = useRef({});
+
+  // const navClick = document.querySelectorAll(".wp-nav-item");
+
+
+  // navClick.addEventListener("click", (e) => {
+  //   console.log("clicked");
+  // })
+
   return (
     <div className="wp-header-container navBar">
 
@@ -12,7 +26,31 @@ export default function WordPressHeader() {
         <nav>
             <ul className="wp-navigation-container">
 
-                <li className="wp-nav-item">
+                {
+                  NavData.map((item) => (
+
+                      <li className="wp-nav-item" key={item.id}>
+
+                        <div style={{fontSize: 24}}>{item.title}
+
+                        <svg className="wp-nav-arrow" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 12 12" fill="none" aria-hidden="true" focusable="false">
+                          <path  d="M1.50002 4L6.00002 8L10.5 4"  
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round">  
+                          </path>
+                        </svg>
+
+                        </div>
+
+                      </li>
+
+                    )
+                  )
+                }
+
+                {/* <li className="wp-nav-item">
                   <div style={{fontSize: 24}}>About CDER 
                   <svg className="wp-nav-arrow" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 12 12" fill="none" aria-hidden="true" focusable="false">
                     <path  d="M1.50002 4L6.00002 8L10.5 4"  
@@ -62,7 +100,7 @@ export default function WordPressHeader() {
                       </path>
                     </svg>
                   </div>
-                </li>
+                </li> */}
 
             </ul>
         </nav>
