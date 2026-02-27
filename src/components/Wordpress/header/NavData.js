16a85171c5bf4b4b5fd55b2e7a153bd643dd9e66
FilipeGrimme <filipe.grimme@hotmail.com>
2026-02-27 13:43:52 -0600

    Updating the navigation bar

---
+++ src/components/Wordpress/header/NavData.js
@@ -0,0 +1,18 @@
+export const NavData = [
+    {
+        title: "About CDER",
+        id: "about-cder"
+    },
+    {
+        title: "Advancing PDC",
+        id: "advancing-pdc"
+    },
+    {
+        title: "Conference Workshops",
+        id: "conference-workshops"
+    },
+    {
+        title: "Community Resources",
+        id: "community-resources"
+    }
+]
